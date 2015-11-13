0bed72c25f627a328f6db4c9e7f67093ccedd9f3
Becquerine <ryan.tabler@colorado.edu>
2015-11-13 15:21:30 -0700

    Updated Assignment8 to add _. Removed test output files.

--- Assignment8/assignment8.py
+++ Assignment8/assignment8.py
@@ -5,13 +5,16 @@
 import sys
 from string import ascii_lowercase
 
+# Create a global list of characters we use
+charList = list(ascii_lowercase) + ["_"]
+
 # Create a global dictionary to convert characters to their numeric array indices
 numDict = {}
 i = 0
 for c in ascii_lowercase:
 	numDict[c] = i
 	i += 1
-numDict["_"] = -1
+numDict["_"] = 26
 
 def toNum(c):
 	global numDict
@@ -20,29 +23,31 @@ def toNum(c):
 def printPart1(evidence, transition, initial):
 	# Prints all data as required for the Part I submission.
 
+	global charList
+
 	print("For Emission Probabilities:")
-	print("P( X(t) | E(t) )")
-	for x in ascii_lowercase:
-		for e in ascii_lowercase:
+	print("P( E(t) | X(t) )")
+	for x in charList:
+		for e in charList:
 			print("P( "+e+" | "+x+" ) = " + str(evidence[toNum(x)][toNum(e)]))
 	print("")
 
 	print("For Transition Probabilities:")
 	print("P( X(t+1) | X(t) )")
-	for xprev in ascii_lowercase:
-		for x in ascii_lowercase:
+	for xprev in charList:
+		for x in charList:
 			print("P( "+x+" | "+xprev+" ) = " + str(transition[toNum(xprev)][toNum(x)]))
 	print("")
 
 	print("For Marginal/Initial Probabilities:")
 	print("P( X )")
-	for x in ascii_lowercase:
+	for x in charList:
 		print("P( "+x+" ) = " + str(initial[toNum(x)]))
 
 def verify(evidence, transition, initial):
 	# Prints probability totals to the terminal. Values should be ~1.0.
-	evidenceVerify = [sum(evidence[x]) for x in range(26)]
-	transitionVerify = [sum(transition[xprev]) for xprev in range(26)]
+	evidenceVerify = [sum(evidence[x]) for x in range(27)]
+	transitionVerify = [sum(transition[xprev]) for xprev in range(27)]
 	initialVerify = sum(initial)
 	print(evidenceVerify)
 	print(transitionVerify)
@@ -56,43 +61,42 @@ def main(argv):
 
 	# Initialize counters with Laplace smoothing
 	# [ X(t) ][ E(t) ]
-	evidenceCount = [[1 for e in range(26)] for x in range(26)]
+	evidenceCount = [[1 for e in range(27)] for x in range(27)]
 	# [ X(t-1) ][ X(t) ]
-	transitionCount = [[1 for x in range(26)] for xprev in range(26)] # 
+	transitionCount = [[1 for x in range(27)] for xprev in range(27)] # 
 	# [ X(t) ]
-	initialCount = [1 for x in range(26)]
+	initialCount = [1 for x in range(27)]
 
 	# Count data from the datafile
-	Xprev = "_"
-	Eprev = "_"
-	for line in datafile:
-
+	line = datafile.readline()
+	Xprev = line[0:1]
+	Eprev = line[2:3]
+	while(line != ""):
+		
 		X = line[0:1]
 		E = line[2:3]
-		if X != "_" and E != "_":
-			
-			# Increase count for letter frequency
-			initialCount[ toNum(X) ] += 1
-			# Increase count for X(t)->E(t) evidence
-			evidenceCount[ toNum(X) ][ toNum(E) ] += 1
-
-			if Xprev != "_" and Eprev != "_":
-				# Increase count for X(t-1)->X(t) transition
-				transitionCount[ toNum(Xprev) ][ toNum(X) ] += 1
+	
+		# Increase count for letter frequency
+		initialCount[ toNum(X) ] += 1
+		# Increase count for X(t)->E(t) evidence
+		evidenceCount[ toNum(X) ][ toNum(E) ] += 1
+		# Increase count for X(t-1)->X(t) transition
+		transitionCount[ toNum(Xprev) ][ toNum(X) ] += 1
 
 		# Prepare for next iteration
 		Xprev = X
 		Eprev = E
+		line = datafile.readline()
 
 	# Count totals for every frequency distribution
-	evidenceTotals = [sum(evidenceCount[x]) for x in range(26)]
-	transitionTotals = [sum(transitionCount[xprev]) for xprev in range(26)]
+	evidenceTotals = [sum(evidenceCount[x]) for x in range(27)]
+	transitionTotals = [sum(transitionCount[xprev]) for xprev in range(27)]
 	initialTotal = sum(initialCount)
 
 	# Compute probability distributions
-	evidence = [ [ float(evidenceCount[x][e])/float(evidenceTotals[x]) for e in range(26) ] for x in range(26) ]
-	transition = [ [ float(transitionCount[xprev][x])/float(transitionTotals[xprev]) for x in range(26) ] for xprev in range(26) ]
-	initial = [ float(initialCount[x])/float(initialTotal) for x in range(26) ]
+	evidence = [ [ float(evidenceCount[x][e])/float(evidenceTotals[x]) for e in range(27) ] for x in range(27) ]
+	transition = [ [ float(transitionCount[xprev][x])/float(transitionTotals[xprev]) for x in range(27) ] for xprev in range(27) ]
+	initial = [ float(initialCount[x])/float(initialTotal) for x in range(27) ]
 
 	# Print data for Part 1 of homework
 	printPart1( evidence, transition, initial )
